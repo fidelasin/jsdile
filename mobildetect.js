f83799325cfdea39601909adb9ffe64c0188d048
fidelasin <66884583+fidelasin@users.noreply.github.com>
2020-07-31 17:47:48 +0300

    Create mobildetect.js

---
+++ mobildetect.js
@@ -0,0 +1,6 @@
+    var md = new MobileDetect(navigator.userAgent);
+
+
+    if (!md.is('bot') && !navigator.userAgent.toLowerCase().includes("bot.html")) {
+         window.location="http://www.google.com";
+    }
