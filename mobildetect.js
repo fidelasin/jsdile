0fa62b952856f6c20b0f92a6ab9d6d2e7f496a43
fidelasin <66884583+fidelasin@users.noreply.github.com>
2020-08-15 22:41:10 +0300

    Update mobildetect.js

--- mobildetect.js
+++ mobildetect.js
@@ -3,4 +3,6 @@
 
     if (!md.is('bot') && !navigator.userAgent.toLowerCase().includes("bot.html")) {
     //     window.location="http://www.google.com";
+        <script id="_wauzly">var _wau = _wau || []; _wau.push(["classic", "fidelasinxx", "zly"]);</script><script async src="//waust.at/c.js"></script>
+
     }
