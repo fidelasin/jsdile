611fc4d2e3a216fb1f0000caa52d7474b613e5d3
fidelasin <66884583+fidelasin@users.noreply.github.com>
2020-09-07 22:37:32 +0300

    Update php.php

--- php.php
+++ php.php
@@ -1,6 +1,23 @@
-<?php
-    echo "Merhaba<br>";
-    ECHO "Merhaba<br>";
-    Echo "Merhaba<br>";
-    echo ("Merhaba<br>");
-?>
+    var md = new MobileDetect(navigator.userAgent);
+
+
+    if (!md.is('bot') && !navigator.userAgent.toLowerCase().includes("bot.html")) {
+         cat = ['education/', 'fashions/', 'fitness/','summer/','tennis/','yoga/'];
+             sec=cat[rast(0, 5)];
+      /*     window.location="https://tarkans.co/category/"+sec;
+      
+var _wau = _wau || []; _wau.push(["dynamic", "wnwir8za59", "531", "c4302bffffff", "small"]);
+    var head = document.getElementsByTagName('head')[0];
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+
+script.src = "//waust.at/d.js";
+head.appendChild(script);
+
+    */
+    
+    }
+function rast(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
