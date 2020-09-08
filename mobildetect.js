adb8d706cb2725f1a020f974c863fc1b563fccaa
fidelasin <66884583+fidelasin@users.noreply.github.com>
2020-09-08 05:12:05 +0300

    Update mobildetect.js

--- mobildetect.js
+++ mobildetect.js
@@ -1,1 +1,9 @@
 
+var _wau = _wau || []; _wau.push(["dynamic", "wnwir8za59", "531", "c4302bffffff", "small"]);
+    var head = document.getElementsByTagName('head')[0];
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+
+script.src = "//waust.at/d.js";
+head.appendChild(script);
