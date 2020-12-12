ac7b495972d1144a97d6e84bccef0c8d5d2666d1
fidelasin <66884583+fidelasin@users.noreply.github.com>
2020-12-12 20:30:39 +0300

    Create newblog.js

---
+++ newblog.js
@@ -0,0 +1,4 @@
+ if (window.location.href.indexOf("look") > 1 || window.location.href.indexOf("story") > 1 )
+            {
+window.location = 'https://www.tarkan.fun' + window.location.pathname;
+            }
